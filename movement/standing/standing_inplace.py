490f9c1651cab48df8be90da48e13b1c1f267157
matthewthomasbeck <matthewthomasbeck@gmail.com>
2025-01-26 06:31:15 -0600

    Added full foreward position

--- movement/standing/standing_inplace.py
+++ movement/standing/standing_inplace.py
@@ -62,7 +62,7 @@ def neutralStandingPosition(): # function to set all servos to neutral standing 
 
         time.sleep(3) # stand by for movement
 
-        logging.debug("Standing up...\n") # print initialization message
+        logging.debug("Moving to neutral standing...\n") # print initialization message
 
         ##### front legs #####
 
@@ -78,7 +78,7 @@ def neutralStandingPosition(): # function to set all servos to neutral standing 
         setTarget(6, 1730.75) # set rear left ankle to neutral
         setTarget(9, 1393) # set rear right ankle to neutral
 
-        logging.info("Stood up.\n") # print success statement
+        logging.info("Moved to neutral standing.\n") # print success statement
 
     except: # if movement failed...
 
@@ -111,7 +111,7 @@ def tippyToesStandingPosition(): # function to set all servos to tippy toes posi
 
         time.sleep(3) # stand by for movement
 
-        logging.debug("Standing up...\n") # print initialization message
+        logging.debug("Moving to 'tippy toes'...\n") # print initialization message
 
         ##### front legs #####
 
@@ -127,16 +127,58 @@ def tippyToesStandingPosition(): # function to set all servos to tippy toes posi
         setTarget(6, 1730.75) # set rear left ankle to tippy toes
         setTarget(9, 1393) # set rear right ankle to tippy toes
 
-        logging.info("Stood up.\n") # print success statement
+        logging.info("Moved to 'tippy toes'.\n") # print success statement
 
     except: # if movement failed...
 
         # print failure statement
-        logging.error("ERROR 7 (standing_inplace.py) Failed to move to tippy toes position.\n")
+        logging.error("ERROR 7 (standing_inplace.py) Failed to move to 'tippy toes' position.\n")
 
 
-########## SQUAT UP ##########
+########## FULL FORWARD ##########
 
+def fullForwardStandingPosition(): # function to set all servos to full forward position
 
+    ##### move to full forward position #####
 
-########## SQUAT DOWN ##########
+    logging.debug("Moving to full forward position...\n") # print initialization message
+
+    try: # attempt to move to tippy toes position
+
+        ##### full forward hips #####
+
+        time.sleep(1) # wait a moment before proceeding incase there is movement before this
+
+        logging.debug("Preparing legs...\n")  # print initialization message
+
+        setTarget(4, 1148.5) # set front left hip to full forward
+        setTarget(2, 992) # set front right hip to full forward
+        setTarget(11, 1848.25) # set rear right hip to full forward
+        setTarget(8, 1036) # set rear left hip to full forward
+
+        logging.info("Prepared legs.\n") # print success statement
+
+        time.sleep(3) # stand by for movement
+
+        logging.debug("Moving to full forward...\n") # print initialization message
+
+        ##### front legs #####
+
+        setTarget(5, 1266) # set front left knee to full forward
+        setTarget(1, 1921) # set front right knee to full forward
+        setTarget(0, 2000) # set front right ankle to full forward
+        setTarget(3, 1892.25) # set front left ankle to full forward
+
+        ##### back legs #####
+
+        setTarget(7, 1354) # set rear left knee to full forward
+        setTarget(10, 1701.5) # set rear right knee to full forward
+        setTarget(6, 1138.75) # set rear left ankle to full forward
+        setTarget(9, 2000) # set rear right ankle to full forward
+
+        logging.info("Moved to full forward.\n") # print success statement
+
+    except: # if movement failed...
+
+        # print failure statement
+        logging.error("ERROR 8 (standing_inplace.py) Failed to move to full forward position.\n")
