a6bc843d1dbe0271bb9c700bcaeb820a8af396b2
matthewthomasbeck <matthewthomasbeck@gmail.com>
2025-09-07 20:53:14 -0500

    Adjusted copyright statement

--- utilities/log.py
+++ utilities/log.py
@@ -1,11 +1,13 @@
 ##################################################################################
-# Copyright (c) 2024 Matthew Thomas Beck                                         #
+# Copyright (c) 2025 Matthew Thomas Beck                                         #
 #                                                                                #
-# All rights reserved. This code and its associated files may not be reproduced, #
-# modified, distributed, or otherwise used, in part or in whole, by any person   #
-# or entity without the express written permission of the copyright holder,      #
-# Matthew Thomas Beck.                                                           #
+# Personal and educational use only. This code and its associated files may be   #
+# copied, modified, and distributed by individuals for non-commercial purposes. #
+# Commercial use by companies or for-profit entities is prohibited.              #
 ##################################################################################
+
+
+
 
 
 ############################################################
